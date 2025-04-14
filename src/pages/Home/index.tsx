3e6ecac105b5980b4c1d9b73821794f2da87a441
MateusResende <mateus.resende.r@gmail.com>
2025-04-14 20:44:14 -0300

    fix: Fix deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -233,7 +233,7 @@ const Home = () => {
             <motion.div 
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
-              className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-16 text-center"
+              className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 mt-16 text-center"
             >
               <div className="p-6 bg-gray-100 rounded-xl">
                 <div className="text-black text-3xl font-bold mb-2">R$14.285,00</div>
